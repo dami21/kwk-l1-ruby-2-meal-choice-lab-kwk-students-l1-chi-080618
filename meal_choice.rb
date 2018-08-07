47c25fd7c01adea7288a72fa7aedf94dbce243a4
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-07 16:14:38 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,19 +2,19 @@ def snacks(your_name)
   "Afternoon is the best time for #{your_name}"
 end 
 
-snacks("cheetos")
+puts snacks("cheetos")
 
 def breakfast(your_name)
   "Morning is the best time for #{your_name}"
 end
 
-breakfast("pancakes")
+puts breakfast("pancakes")
 
 def lunch(your_name)
   "lunch is the best timne for #{your_name}"
 end
 
-lunch("grilled_cheese")
+puts lunch("grilled_cheese")
 
 def dinner(your_name)
  "dinner is the best time for #{your_name}"
